b4858186fbdfbc425f55cbb96825777ed23fc5ea
albertog <alberto.garcia@globalcorporation.cc>
2020-01-17 16:04:24 -0600

    Initial Commit

---
+++ SistemaDeMonitoreoYControl-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SistemaDeMonitoreoYControl-Swift
+//
+//  Created by Diseno2 on 1/17/20.
+//  Copyright © 2020 GlobalCorporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
